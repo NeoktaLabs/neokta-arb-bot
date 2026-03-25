181cd1fd0185ebd92f2dee7fc9c44235e4a00c58
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-03-25 17:15:23 +0100

    Update curve.types.ts

--- src/integrations/curve/curve.types.ts
+++ src/integrations/curve/curve.types.ts
@@ -1,28 +1,31 @@
 // src/integrations/curve/curve.types.ts
 
+import type { Address } from "../../domain/types";
+
 export interface CurvePoolConfig {
-  address: string;
+  address: Address;
   name: string;
 }
 
 export interface CurvePoolCoin {
   index: number;
-  address: string;
+  address: Address;
   symbol: string;
   decimals: number;
 }
 
 export interface CurvePoolSnapshot {
-  poolAddress: string;
+  poolAddress: Address;
   coins: CurvePoolCoin[];
   balances: number[];
 }
 
 export interface DiscoveredCurvePool {
   name: string;
-  address: string;
+  address: Address;
   coins: CurvePoolCoin[];
   balances: number[];
   hasUsdc: boolean;
   isTwoCoinPool: boolean;
+  usdcCoinAddress: Address | null;
 }
