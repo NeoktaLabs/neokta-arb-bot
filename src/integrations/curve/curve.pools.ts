f7a2df8e0c22155626c30bddda851badc161b349
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-03-25 13:32:20 +0100

    Update curve.pools.ts

--- src/integrations/curve/curve.pools.ts
+++ src/integrations/curve/curve.pools.ts
@@ -4,13 +4,55 @@ import type { CurvePoolConfig } from "./curve.types";
 
 export const CURVE_POOLS: CurvePoolConfig[] = [
   {
-    name: "mBASIS/USDC",
+    name: "Curve Pool 1",
     address: "0x0714027e44802b2ff76389daf5371990cc3a4c24",
   },
-
-  // Add more pools here later
-  // {
-  //   name: "Another Pool",
-  //   address: "0x...",
-  // },
+  {
+    name: "Curve Pool 2",
+    address: "0x5d37f9b272ca7cda2a05245b9a503746eefac88f",
+  },
+  {
+    name: "Curve Pool 3",
+    address: "0x74d80ee400d3026fdd2520265cc98300710b25d4",
+  },
+  {
+    name: "Curve Pool 4",
+    address: "0x942644106b073e30d72c2c5d7529d5c296ea91ab",
+  },
+  {
+    name: "Curve Pool 5",
+    address: "0x95af759ec2f4385edbbba959a8a1cdc65610d080",
+  },
+  {
+    name: "Curve Pool 6",
+    address: "0x269b47978f4348c96f521658ef452ff85906fcfe",
+  },
+  {
+    name: "Curve Pool 7",
+    address: "0x2d84d79c852f6842abe0304b70bbaa1506add457",
+  },
+  {
+    name: "Curve Pool 8",
+    address: "0x1e8d78e9b3f0152d54d32904b7933f1cfe439df1",
+  },
+  {
+    name: "Curve Pool 9",
+    address: "0xd5e41fcfbcf3a9cc4cc88fd4176106a24899d188",
+  },
+  {
+    name: "Curve Pool 10",
+    address: "0x3183f5956a7b8cbaacea34401e227af1c6df6d34",
+  },
+  {
+    name: "Curve Pool 11",
+    address: "0x8e3da27b6496ca4373ad8261835f56d0107c8e25",
+  },
+  {
+    name: "Curve Pool 12",
+    address: "0xe080d14bf6ecec4c48ebe11055d4dea5dbc30e41",
+  },
+  {
+    name: "Curve Pool 13",
+    address: "0x78051fbf40581619ffaadb6cd7e5856d4a327a6d",
+  },
 ];
