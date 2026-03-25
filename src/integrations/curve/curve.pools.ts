faba086f8d308e3257f8537654f59a7c44482114
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-03-25 11:06:59 +0100

    Update curve.pools.ts

--- src/integrations/curve/curve.pools.ts
+++ src/integrations/curve/curve.pools.ts
@@ -4,7 +4,13 @@ import type { CurvePoolConfig } from "./curve.types";
 
 export const CURVE_POOLS: CurvePoolConfig[] = [
   {
-    name: "Curve Etherlink Test Pool",
+    name: "mBASIS/USDC",
     address: "0x0714027e44802b2ff76389daf5371990cc3a4c24",
   },
+
+  // Add more pools here later
+  // {
+  //   name: "Another Pool",
+  //   address: "0x...",
+  // },
 ];
