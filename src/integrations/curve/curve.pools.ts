e8d731ec9ff5ec46a5d90216a60fb0b2b20a8926
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-03-25 21:10:43 +0100

    Update curve.pools.ts

--- src/integrations/curve/curve.pools.ts
+++ src/integrations/curve/curve.pools.ts
@@ -4,55 +4,55 @@ import type { CurvePoolConfig } from "./curve.types";
 
 export const CURVE_POOLS: CurvePoolConfig[] = [
   {
-    name: "Curve Pool 1",
+    name: "mBASIS/USDC",
     address: "0x0714027e44802b2ff76389daf5371990cc3a4c24",
   },
   {
-    name: "Curve Pool 2",
+    name: "mRE7/USDC",
     address: "0x5d37f9b272ca7cda2a05245b9a503746eefac88f",
   },
   {
-    name: "Curve Pool 3",
+    name: "stXTZ/WXTZ",
     address: "0x74d80ee400d3026fdd2520265cc98300710b25d4",
   },
   {
-    name: "Curve Pool 4",
+    name: "mTBILL/USDC",
     address: "0x942644106b073e30d72c2c5d7529d5c296ea91ab",
   },
   {
-    name: "Curve Pool 5",
+    name: "USDSM/USDC",
     address: "0x95af759ec2f4385edbbba959a8a1cdc65610d080",
   },
   {
-    name: "Curve Pool 6",
+    name: "mMEV/USDC",
     address: "0x269b47978f4348c96f521658ef452ff85906fcfe",
   },
   {
-    name: "Curve Pool 7",
+    name: "USDC/USDT",
     address: "0x2d84d79c852f6842abe0304b70bbaa1506add457",
   },
   {
-    name: "Curve Pool 8",
+    name: "LBTC/WBTC",
     address: "0x1e8d78e9b3f0152d54d32904b7933f1cfe439df1",
   },
   {
-    name: "Curve Pool 9",
+    name: "USDC/USDtz",
     address: "0xd5e41fcfbcf3a9cc4cc88fd4176106a24899d188",
   },
   {
-    name: "Curve Pool 10",
+    name: "XU308/USDC",
     address: "0x3183f5956a7b8cbaacea34401e227af1c6df6d34",
   },
   {
-    name: "Curve Pool 11",
+    name: "XU308/WXTZ",
     address: "0x8e3da27b6496ca4373ad8261835f56d0107c8e25",
   },
   {
-    name: "Curve Pool 12",
+    name: "XU308/WBTC",
     address: "0xe080d14bf6ecec4c48ebe11055d4dea5dbc30e41",
   },
   {
-    name: "Curve Pool 13",
+    name: "USDC/WXTZ",
     address: "0x78051fbf40581619ffaadb6cd7e5856d4a327a6d",
   },
 ];
