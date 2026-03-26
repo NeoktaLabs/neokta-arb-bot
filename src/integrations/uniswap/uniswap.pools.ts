d646481e065c4feaa6fc66d54d643163f707ea81
Julien - NeoktaLabs <74187584+NeoktaLabs@users.noreply.github.com>
2026-03-26 16:26:53 +0100

    Update uniswap.pools.ts

--- src/integrations/uniswap/uniswap.pools.ts
+++ src/integrations/uniswap/uniswap.pools.ts
@@ -1,23 +1,30 @@
 // src/integrations/uniswap/uniswap.pools.ts
 
-import type { Address } from "../../domain/types";
+import type { UniswapPoolSeed } from "./uniswap.types";
 
-export interface UniswapPoolConfig {
-  address: Address;
-  name: string;
-}
-
-export const ETHEREUM_UNISWAP_POOLS: UniswapPoolConfig[] = [
+export const ETHEREUM_UNISWAP_POOLS: UniswapPoolSeed[] = [
   {
     name: "Uniswap USDC/USDT 0.01%",
-    address: "0x3416cF6C708Da44DB2624D63ea0AAef7113527C6",
+    token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    token1: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    fee: 100,
   },
   {
     name: "Uniswap USDC/WETH 0.05%",
-    address: "0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640",
+    token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    token1: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    fee: 500,
   },
   {
-    name: "Uniswap WBTC/USDC",
-    address: "0x99ac8cA7087fA4A2A1FB6357269965A2014ABc35",
+    name: "Uniswap USDC/WETH 0.3%",
+    token0: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    token1: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    fee: 3000,
+  },
+  {
+    name: "Uniswap WBTC/USDC 0.3%",
+    token0: "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",
+    token1: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    fee: 3000,
   },
 ];
